b59f83f2c0f5909d445791e30ec1f3d90f646852
Juboraj Sujon <mdsujon72@gmail.com>
2024-08-15 11:32:56 +0600

    Added theme controller

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import ThemeController from "../components/ThemeController/ThemeController";
 
 const Main = () => {
   return (
     <div className="font-poppins">
-      <Outlet />
+      <Navbar />
+      <div className="dark:bg-slate-800 min-h-[calc(100vh-110px)] overflow-x-hidden">
+        <Outlet />
+        <ThemeController />
+      </div>
       <Footer />
     </div>
   );
